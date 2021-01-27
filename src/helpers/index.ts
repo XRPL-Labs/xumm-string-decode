74bf5bbd38f7d3a0cc1bb74986cad548fc24933a
Wietse Wind <mail@wietse.com>
2021-01-27 14:03:35 +0100

    Fix for KaiOS

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 const tryUrlParams = <T>(url: URLSearchParams, find: Array<string>, cast: Function): string | undefined | T => {
   let value: string | T = undefined
-  for (const s in find) {
+  for (let s in find) {
     if (url.get(find[s]) !== null) {
       value = cast(url.get(find[s]))
       break
