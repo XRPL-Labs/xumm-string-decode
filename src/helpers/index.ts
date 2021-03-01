1dcdc2fedcf9797e4bb624276ce2661100177b43
Wietse Wind <mail@wietse.com>
2021-03-01 14:16:16 +0100

    Disable line (needed for KaiOS)

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 const tryUrlParams = <T>(url: URLSearchParams, find: Array<string>, cast: Function): string | undefined | T => {
   let value: string | T = undefined
+  // tslint:disable-next-line
   for (let s in find) {
     if (url.get(find[s]) !== null) {
       value = cast(url.get(find[s]))
